07dc356a228a7d02941703cb75f58dd47b441dbc
Evan McCoy <qwertynessthedev@gmail.com>
2017-08-01 19:30:29 -0400

    Added moveable function and potentiometer limits.

--- arms.py
+++ arms.py
@@ -1,6 +1,12 @@
 import maestro
 import protocol
 
+#Potentiometer limits for the different joints, lowest first
+RIGHT_SHOULDER_LIMITS = [0, 0]
+LEFT_SHOULDER_LIMITS = [0, 0]
+RIGHT_ELBOW_LIMITS = [0, 0]
+LEFT_ELBOW_LIMITS = [0, 0]
+
 class Arms:
     # Shoulder: (Maestro channel, analog channel) | Hand: Maestro channel | Elbow: (Maestro channel, analog channel)
     def __init__(self, ch_left_shoulder=(2,0), ch_right_shoulder=(3,0), ch_left_elbow=(0,2), ch_right_elbow=(1,3), ch_left_hand=4, ch_right_hand=5):
@@ -12,6 +18,19 @@ class Arms:
         self.right_hand = maestro.Controller(ch_right_hand)
 
 class Joint:
-    def __init__(self, maestro_channel, pot_channel):
+    def __init__(self, maestro_channel, pot_channel=None, limits=None):
         self.controller = maestro.Controller(channel)
-        self.pot = protocol.Potentiometer(pot_channel)
+        if not pot_channel == None:
+            self.pot = protocol.Potentiometer(pot_channel)
+        self.limits = limits
+
+    def moveable(self):
+        if self.limits == None:
+            return True
+        if self.pot == None:
+            return False
+
+        position = self.pot.read()
+        if position <= self.limits[1] and position >= self.limits[0]:
+            return True
+        return False
